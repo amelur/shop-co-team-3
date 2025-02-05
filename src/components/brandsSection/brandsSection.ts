27df0abb815e8ea102c767707d68f4c9a04ea1aa
Maksim28108 <bilukha.maksim@student.ehu.lt>
2025-02-05 22:09:07 +0200

    fix1

--- src/components/brandsSection/brandsSection.ts
+++ src/components/brandsSection/brandsSection.ts
@@ -1,5 +1,4 @@
-import './brandsSection.css';
-
+import "./brandsSection.css";
 
 export interface Brand {
   id: number;
@@ -10,66 +9,25 @@ export interface Brand {
 }
 
 export const brandsData: Brand[] = [
-  {
-    id: 0,
-    brandName: 'Versace',
-    src: '../../src/assets/images/brandVersace.png',
-    alt: 'Versace brand image',
-    link: './src/pages/CategoryPage/categoryPage.html',
-  },
-  {
-    id: 1,
-    brandName: 'Zara',
-    src: '../../src/assets/images/brandZara.png',
-    alt: 'Zara brand image',
-    link: '',
-  },
-  {
-    id: 2,
-    brandName: 'Gucci',
-    src: '../../src/assets/images/brandGucci.png',
-    alt: 'Gucci brand image',
-    link: '',
-  },
-  {
-    id: 3,
-    brandName: 'Prada',
-    src: '../../src/assets/images/brandPrada.png',
-    alt: 'Prada brand image',
-    link: '',
-  },
-  {
-    id: 4,
-    brandName: 'Calvin Klein',
-    src: '../../src/assets/images/brandCalvin_Klein.png',
-    alt: 'Calvin Klein brand image',
-    link: '',
-  },
+  { id: 0, brandName: "Versace", src: "../../src/assets/images/brandVersace.png", alt: "Versace brand image", link: "./src/pages/CategoryPage/categoryPage.html" },
+  { id: 1, brandName: "Zara", src: "../../src/assets/images/brandZara.png", alt: "Zara brand image", link: "" },
+  { id: 2, brandName: "Gucci", src: "../../src/assets/images/brandGucci.png", alt: "Gucci brand image", link: "" },
+  { id: 3, brandName: "Prada", src: "../../src/assets/images/brandPrada.png", alt: "Prada brand image", link: "" },
+  { id: 4, brandName: "Calvin Klein", src: "../../src/assets/images/brandCalvin_Klein.png", alt: "Calvin Klein brand image", link: "" },
 ];
 
-
-export function addBrandsSection(data: Brand[]) {
-  const brandSection = document.createElement('section');
-  brandSection.className = 'homePage_brandSection';
-
-  data.forEach((item: Brand) => {
-    const img = document.createElement('img');
-    img.src = item.src;
-    img.alt = item.alt;
-    img.title = item.brandName;
-    img.classList.add('brandImg');
-
-    const link = document.createElement('a');
-    link.href = item.link;
-    link.target = '_blank';
-    link.appendChild(img);
-    brandSection.appendChild(link);
-  });
-
-  return brandSection;
+export function createBrandSection(): string {
+  return `
+    <section class="homePage_brandSection">
+      ${brandsData.map(createBrandItem).join("")}
+    </section>
+  `;
 }
 
-export function createBrandSection() {
-  const brandSection = addBrandsSection(brandsData);
-  return brandSection.outerHTML;
+function createBrandItem(brand: Brand): string {
+  return `
+    <a href="${brand.link}" target="_blank">
+      <img src="${brand.src}" alt="${brand.alt}" title="${brand.brandName}" class="brandImg" />
+    </a>
+  `;
 }
--- src/components/hero/hero.ts
+++ src/components/hero/hero.ts
@@ -1,41 +1,97 @@
-import './hero.css';
-import { renderButton } from '../buttons/button';
+// import './hero.css';
+// import { renderButton } from '../buttons/button';
+
+// export function sectionHero(): string {
+//   const heroButton = renderButton('Shop Now', 'hero__button', '#categories');
+
+//   const template = `<section class="hero"> 
+//     <div class="hero__wrapper" id="hero">
+//       <h1 class="hero__title">
+//         FIND <span class="underline">ANYTHING</span> THAT MATCHES YOUR STYLE
+//       </h1>
+//       <p class="hero__subtitle">Browse through our diverse range of meticulously crafted garments, designed to bring out
+//         your individuality and cater to your sense of style.</p>
+//       <a href="#categories">${heroButton.outerHTML}</a>
+//       <div class="hero__list">
+//         <div class="hero__item">
+//           <p class="hero__quantity">200+</p>
+//           <p class="hero__text">International Brands</p>
+//         </div>
+//         <div class="hero__line"></div>
+//         <div class="hero__item">
+//           <p class="hero__quantity">2,000+</p>
+//           <p class="hero__text">High-Quality Products</p>
+//         </div>
+//         <div class="hero__line hero__line_none"></div>
+//         <div class="hero__item">
+//           <p class="hero__quantity">30,000+</p>
+//           <p class="hero__text">Happy Customers</p>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="hero__img">
+
+//       <div class="rhombus rhombus_1"></div>
+//       <div class="rhombus rhombus_2"></div>
+//     </div>
+//   </section>
+//   `;
+
+//   return template;
+// }
+
+
+
+import "./hero.css";
+import { renderButton } from "../buttons/button";
 
 export function sectionHero(): string {
-  const heroButton = renderButton('Shop Now', 'hero__button', '#categories');
+  const heroButton = renderButton("Shop Now", "hero__button", "#categories");
 
-  const template = `<section class="hero"> 
-    <div class="hero__wrapper" id="hero">
-      <h1 class="hero__title">
-        FIND <span class="underline">ANYTHING</span> THAT MATCHES YOUR STYLE
-      </h1>
-      <p class="hero__subtitle">Browse through our diverse range of meticulously crafted garments, designed to bring out
-        your individuality and cater to your sense of style.</p>
-      <a href="#categories">${heroButton.outerHTML}</a>
-      <div class="hero__list">
-        <div class="hero__item">
-          <p class="hero__quantity">200+</p>
-          <p class="hero__text">International Brands</p>
-        </div>
-        <div class="hero__line"></div>
-        <div class="hero__item">
-          <p class="hero__quantity">2,000+</p>
-          <p class="hero__text">High-Quality Products</p>
-        </div>
-        <div class="hero__line hero__line_none"></div>
-        <div class="hero__item">
-          <p class="hero__quantity">30,000+</p>
-          <p class="hero__text">Happy Customers</p>
-        </div>
+  return `
+    <section class="hero"> 
+      <div class="hero__wrapper" id="hero">
+        <h1 class="hero__title">
+          FIND <span class="underline">ANYTHING</span> THAT MATCHES YOUR STYLE
+        </h1>
+        <p class="hero__subtitle">
+          Browse through our diverse range of meticulously crafted garments, 
+          designed to bring out your individuality and cater to your sense of style.
+        </p>
+        <a href="#categories">${heroButton.outerHTML}</a>
+        ${createHeroList()}
       </div>
-    </div>
-    <div class="hero__img">
+      ${createHeroImage()}
+    </section>
+  `;
+}
 
+function createHeroList(): string {
+  return `
+    <div class="hero__list">
+      ${createHeroItem("200+", "International Brands")}
+      <div class="hero__line"></div>
+      ${createHeroItem("2,000+", "High-Quality Products")}
+      <div class="hero__line hero__line_none"></div>
+      ${createHeroItem("30,000+", "Happy Customers")}
+    </div>
+  `;
+}
+
+function createHeroItem(quantity: string, text: string): string {
+  return `
+    <div class="hero__item">
+      <p class="hero__quantity">${quantity}</p>
+      <p class="hero__text">${text}</p>
+    </div>
+  `;
+}
+
+function createHeroImage(): string {
+  return `
+    <div class="hero__img">
       <div class="rhombus rhombus_1"></div>
       <div class="rhombus rhombus_2"></div>
     </div>
-  </section>
   `;
-
-  return template;
 }
